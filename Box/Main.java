311da3e4671cb8e99923ede4ffb5e18f24a6e807
Reeja20040923 <reejayaramalla@gmail.com>
2025-08-19 19:49:44 +0530

    OOPS IN JAVA

---
+++ Box/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Box box = new Box(4);
+
+        System.err.println(box.l+" "+box.w+" "+box.h);
+
+    }
+    
+}
---
+++ loops.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class loops {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String fruit = in.next();
+        String veg = in.next();
+
+        if(fruit.equals(veg)){
+            System.out.println("same");
+        }
+
+
+    }
+    
+}
